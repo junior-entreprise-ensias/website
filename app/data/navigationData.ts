ca4d67ce410b14d4465d3ddf91c6fb3f1f2f4a57
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-10-20 01:59:58 +0100

    Add navigationData.ts file with navigation items

---
+++ app/data/navigationData.ts
@@ -0,0 +1,10 @@
+import { NavigationItem } from "@/app/types/navigation";
+
+export const navigationData: NavigationItem[] = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'About', href: '#about', current: false },
+  { name: 'Services', href: '#services', current: false },
+  { name: 'Projects', href: '#projects', current: false },
+  { name: 'Team', href: '#team', current: false },
+  { name: 'Contact', href: '#contact', current: false },
+];
