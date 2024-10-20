dd42c5cbaf0114cf521649ef5009aa9630ce8303
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-10-20 18:16:50 +0100

    Add footerData.ts

---
+++ app/data/footerData.ts
@@ -0,0 +1,12 @@
+import { FooterItem } from "../types/footer";
+
+export const footerData: FooterItem[] = [
+  {
+    id: 1,
+    link: ['Home', 'About', 'Services', 'Projects'],
+  },
+  {
+    id: 2,
+    link: ['Team', 'Contact', 'Privacy policy', 'Terms & conditions'],
+  },
+];
