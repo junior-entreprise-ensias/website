e375159ed09cd284c689288d441ef02e1b89e9b9
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-10-20 02:00:49 +0100

    Refactor Navbar Drawerdata component to use navigationData from external file

--- app/components/Navbar/Drawerdata.tsx
+++ app/components/Navbar/Drawerdata.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import Link from "next/link";
+import { navigationData } from "../../data/navigationData";
+import SocialMedia from "./SocialMedia";
 
-interface NavigationItem {
-  name: string;
-  href: string;
-  current: boolean;
-}
-
-const navigation: NavigationItem[] = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'Services', href: '#services', current: false },
-  { name: 'About', href: '#about', current: false },
-  { name: 'Project', href: '#project', current: false },
-  { name: 'Help', href: '/', current: false },
-]
+const navigation = navigationData;
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 const Data = () => {
@@ -30,26 +20,23 @@ const Data = () => {
                 key={item.name}
                 href={item.href}
                 className={classNames(
-                  item.current ? 'text-black hover:opacity-100' : 'hover:text-black hover:opacity-100',
-                  'px-2 py-1 text-lg font-normal opacity-75 block'
+                  item.current
+                    ? "text-black hover:opacity-100"
+                    : "hover:text-black hover:opacity-100",
+                  "px-2 py-1 text-lg font-normal opacity-75 block"
                 )}
-                aria-current={item.current ? 'page' : undefined}
+                aria-current={item.current ? "page" : undefined}
               >
                 {item.name}
               </Link>
             ))}
             <div className="mt-4"></div>
-            <button className="bg-white w-full text-blue border border-lightblue font-medium py-2 px-4 rounded">
-              Sign In
-            </button>
-            <button className="bg-lightblue w-full hover:bg-blue hover:text-white text-blue font-medium my-2 py-2 px-4 rounded">
-              Sign up
-            </button>
+            <SocialMedia />
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Data;
