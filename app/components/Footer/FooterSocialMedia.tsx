523515d825fc13a94bd70ae72645f7c88093bbae
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-10-20 18:16:33 +0100

    Refactor FooterSocialMedia component to use react-icons package for social media icons

---
+++ app/components/Footer/FooterSocialMedia.tsx
@@ -0,0 +1,31 @@
+import { FaLinkedinIn, FaFacebookF, FaInstagram } from "react-icons/fa";
+// import { FaXTwitter } from "react-icons/fa6";
+// import { FiGithub } from "react-icons/fi";
+import Link from "next/link";
+
+const FooterSocialMedia = () => {
+  return (
+    <div className="flex gap-4">
+      <Link
+        href="https://www.linkedin.com/"
+        className="flex items-center justify-center text-xl p-3.5 bg-bluebg text-white rounded-full"
+      >
+        <FaLinkedinIn />
+      </Link>
+      <Link
+        href="https://www.facebook.com/"
+        className="flex items-center justify-center text-xl p-3.5 bg-bluebg text-white rounded-full"
+      >
+        <FaFacebookF />
+      </Link>
+      <Link
+        href="https://www.instagram.com/"
+        className="flex items-center justify-center text-xl p-3.5 bg-bluebg text-white rounded-full"
+      >
+        <FaInstagram />
+      </Link>
+    </div>
+  );
+};
+
+export default FooterSocialMedia;
