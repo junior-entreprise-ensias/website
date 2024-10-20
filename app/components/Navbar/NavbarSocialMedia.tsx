e547c6d50c62b157a72c4c3da7e473195f0d4099
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-10-20 18:16:24 +0100

    Refactor NavbarSocialMedia component to use react-icons package for social media icons

---
+++ app/components/Navbar/NavbarSocialMedia.tsx
@@ -0,0 +1,31 @@
+import { FaLinkedinIn, FaFacebookF, FaInstagram } from "react-icons/fa";
+// import { FaXTwitter } from "react-icons/fa6";
+// import { FiGithub } from "react-icons/fi";
+import Link from "next/link";
+
+const NavbarSocialMedia = () => {
+  return (
+    <div className="flex gap-4">
+      <Link
+        href="https://www.linkedin.com/"
+        className="flex items-center justify-center w-8 h-8 bg-blue text-white rounded-full"
+      >
+        <FaLinkedinIn />
+      </Link>
+      <Link
+        href="https://www.facebook.com/"
+        className="flex items-center justify-center w-8 h-8 bg-blue text-white rounded-full"
+      >
+        <FaFacebookF />
+      </Link>
+      <Link
+        href="https://www.instagram.com/"
+        className="flex items-center justify-center w-8 h-8 bg-blue text-white rounded-full"
+      >
+        <FaInstagram />
+      </Link>
+    </div>
+  );
+};
+
+export default NavbarSocialMedia;
