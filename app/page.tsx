78745b8be8e4d57d92a72141409ab035b48e5b5f
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-10-20 18:17:28 +0100

    Refactor Home component to comment out Newsletter component

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       {/* <Why /> */}
       {/* <Network /> */}
       {/* <Clientsay /> */}
-      <Newsletter />
+      {/* <Newsletter /> */}
     </main>
   )
 }
