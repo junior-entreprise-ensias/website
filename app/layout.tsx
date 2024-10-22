f2b282739e0e850e7ea42023eaf9c94541dcb915
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-10-22 18:03:41 +0100

    Add favicon files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,44 @@
-import './globals.css';
-import Navbar from './components/Navbar/index';
-import Footer from './components/Footer/Footer';
-
+import "./globals.css";
+import Navbar from "./components/Navbar/index";
+import Footer from "./components/Footer/Footer";
 
 export const metadata = {
-  title: 'ENSIAS Junior Entreprise',
-  description: 'ENSIAS Junior Entreprise Website',
-}
+  title: "ENSIAS Junior Entreprise",
+  description: "ENSIAS Junior Entreprise Website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon_io/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+      </head>
       <body>
         <Navbar />
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
